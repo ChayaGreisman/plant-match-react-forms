074591d7628d6d7f78ff8f4f9077a44dfa7e832c
ChayaGreisman <chayamgreisman@gmail.com>
2020-07-15 12:37:16 -0400

    core deliverables

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -7,9 +7,13 @@ const API_BASE = 'http://localhost:3001/plants'
 class HomeView extends React.Component {
     state = {
         plants: [],
+        search: "",
         showCreateForm: false,
     }
 
+    handleSearchChange = e => {
+        this.setState({search: e.target.value})
+    }
     /** DO NOT WORRY ABOUT THIS COMPONENTDIDMOUNT METHOD
      * It's part of content we'll learn on FRIDAY of WEEK ONE
     */
@@ -32,16 +36,17 @@ class HomeView extends React.Component {
         const { plants, showCreateForm } = this.state
         // TODO: In order to search, what state, methods and element attributes are needed? 
         // In order to render the correct plants, what calculations do you need to do and what props do you need to change below?
-
+        let filteredPlants = this.state.plants.filter(plant=>plant.Common_Name.toLowerCase().includes(this.state.search.toLowerCase()))
         return (
             <div>
                 <button onClick={this.toggleCreateForm}>{showCreateForm ? "Hide Form" : "Submit Plant"}</button>
                 { showCreateForm && <CreatePlantForm />}
                 <hr />
                 <div>
-                    <input placeholder="Search for Plants"/>
+                    <input placeholder="Search for Plants" onChange={this.handleSearchChange}/>
                 </div>
-                <MatchContainer plants={plants}/>
+                {/* change to filtered plants below */}
+                <MatchContainer plants={filteredPlants}/>
             </div>
         )
     }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,38 @@
 import React from 'react';
 
 class Login extends React.Component {
+
     state = {
+        name: "",
+        username: "",
+        password: "",
+        confirmPassword: ""
+        
         // TODO: What needs to be represented in state for a fully controlled form?
     }
     // TODO: What methods need to be created for a fully controlled form?
+    handleChange = e => {
+        this.setState(
+            {[e.target.name]: e.target.value}
+        )
+        console.log(this.state)
+    }
+
+    handleSubmit = e => {
+        this.props.changeView('home')
+    }
     // HINT: Use the line below to change the view when the form is submitted
     // this.props.changeView('home')
 
     render(){
         // TODO: What additional attributes and event handlers are needed on each of the elements below?
         return (
-            <form className="vertical-flex">
+            <form className="vertical-flex" >
                 <h2>Create an Account</h2>
-                <input placeholder="Name"/>
-                <input placeholder="Username"/>
-                <input placeholder="Password"/>
-                <input placeholder="Confirm Password"/>
+                <input name="name" placeholder="Name" value= {this.state.name} onChange={this.handleChange}/>
+                <input name="username" placeholder="Username" value= {this.state.username} onChange={this.handleChange}/>
+                <input type="password" name="password" placeholder="Password" value= {this.state.password} onChange={this.handleChange}/>
+                <input type="password" name="confirmPassword" placeholder="Confirm Password" value= {this.state.confirmPassword} onChange={this.handleChange}/>
                 <button type="submit">Submit</button>
             </form>
         )
